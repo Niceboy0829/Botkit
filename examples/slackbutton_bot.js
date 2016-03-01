53a0fe804efc78c48a30fcd0fa721d27ead42ca2
Niceboy0829 <nice1qazjay@gmail.com>
2016-02-29 21:06:59 -0600

    Added example link to sentiment_analysis

--- examples/slackbutton_bot.js
+++ examples/slackbutton_bot.js
@@ -127,7 +127,7 @@ controller.storage.teams.all(function(err,teams) {
   // connect all teams with bots up to slack!
   for (var t  in teams) {
     if (teams[t].bot) {
-      controller.spawn(teams[t]).startRTM(function(err, bot) {
+      var bot = controller.spawn(teams[t]).startRTM(function(err) {
         if (err) {
           console.log('Error connecting bot to Slack:',err);
         } else {
