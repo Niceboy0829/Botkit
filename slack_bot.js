8184f09757f2d304d046ad0dddc4997a75410a08
Niceboy0829 <nice1qazjay@gmail.com>
2017-01-09 12:56:48 -0600

    Start Cisco Spark bot's tick immediately

--- slack_bot.js
+++ slack_bot.js
@@ -80,6 +80,7 @@ var bot = controller.spawn({
     token: process.env.token
 }).startRTM();
 
+
 controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
 
     bot.api.reactions.add({
