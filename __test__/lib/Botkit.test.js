a1058045b149f347fb2de9b9ce7cccd4072186db
Niceboy0829 <nice1qazjay@gmail.com>
2017-06-28 19:00:01 +0100

    Assign uid to hello message

--- __test__/lib/Botkit.test.js
+++ __test__/lib/Botkit.test.js
@@ -1,12 +1,9 @@
-'use strict';
-
 let botkit;
 
 jest.mock('../../lib/CoreBot', () => 'corebot');
 jest.mock('../../lib/SlackBot', () => 'slackbot');
 jest.mock('../../lib/Facebook', () => 'facebook');
-jest.mock('../../lib/TwilioIPMBot', () => 'twilioipm');
-jest.mock('../../lib/TwilioSMSBot', () => 'twiliosms');
+jest.mock('../../lib/TwilioIPMBot', () => 'twilio');
 jest.mock('../../lib/BotFramework', () => 'botframework');
 jest.mock('../../lib/CiscoSparkbot', () => 'spark');
 jest.mock('../../lib/ConsoleBot', () => 'console');
@@ -20,8 +17,7 @@ test('exports bot interfaces', () => {
     expect(botkit.core).toBe('corebot');
     expect(botkit.slackbot).toBe('slackbot');
     expect(botkit.facebookbot).toBe('facebook');
-    expect(botkit.twilioipmbot).toBe('twilioipm');
-    expect(botkit.twiliosmsbot).toBe('twiliosms');
+    expect(botkit.twilioipmbot).toBe('twilio');
     expect(botkit.botframeworkbot).toBe('botframework');
     expect(botkit.sparkbot).toBe('spark');
     expect(botkit.consolebot).toBe('console');
