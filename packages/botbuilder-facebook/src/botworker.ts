0fc229a5944bb484f0df51b8e9e753f1d54ab481
Niceboy0829 <nice1qazjay@gmail.com>
2019-03-13 18:42:14 -0400

    Added Slack chat.scheduleMessage to web api

--- packages/botbuilder-facebook/src/botworker.ts
+++
@@ -1,9 +0,0 @@
-import { BotWorker, BotkitMessage } from 'botkit';
-
-export class FacebookWorker extends BotWorker {
-
-    constructor(botkit, config) {
-        super(botkit, config);
-    }
-
-}
