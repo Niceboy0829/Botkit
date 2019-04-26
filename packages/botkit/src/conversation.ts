3c2de4fd9db726b9c47f35f1d8e52b3be891b773
Niceboy0829 <nice1qazjay@gmail.com>
2019-04-26 16:51:35 +0300

    Fix signature of say and reply methods

--- packages/botkit/src/conversation.ts
+++ packages/botkit/src/conversation.ts
@@ -33,7 +33,7 @@ interface BotkitMessageTemplate {
     text: string[];
     action?: string;
     quick_replies?: [any];
-    attachments?: any[];
+    attachments?: [];
     channelData?: any;
     collect: {
         key: string;
